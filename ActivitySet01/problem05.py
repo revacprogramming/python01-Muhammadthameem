9dc6d2220d20c6c0c72e88cffd1b862b988981ed
Muhammadthameem <94587542+Muhammadthameem@users.noreply.github.com>
2022-05-17 17:05:21 +0530

    Update problem05.py

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -1,12 +1,16 @@
-# Functions
+a= float(input("Enter Score: "))
+if ((a<0.0) or (a>1.0)) :
+    print("Error")
+elif a>=0.9:
+    print("A")
+elif a>=0.8:
+    print("B")
+elif a>=0.7:
+    print("C")
+elif a>=0.6:
+    print("D")
+elif a<0.6:
+    print("F")
+    
 
 
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
